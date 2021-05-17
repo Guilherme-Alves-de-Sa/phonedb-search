f0b157b290595892a9580e1463ea18ae1a85ae0b
Guilherme Sa <alvesdesa@hotmail.com>
2021-05-17 21:56:04 +0100

    removi o lixo

--- phoneScript.php
+++ phoneScript.php
@@ -4,31 +4,26 @@ require_once "curlPhone.php";
 
 class phoneScript
 {
-    private $devicesFile
-
-    public function __construct()
+    // Scrapping das páginas da phonedb.net
+    public function runJsonForPhoneDB($filePath)
     {
         // ficheiro json na mesma diretoria do script com os modelos
-        $str = file_get_contents('./devices.json');
+        $devicesFile = file_get_contents($filePath);
 
-// passar a informação do ficheiro .json para uma variável
-        $list = json_decode($str, true);
+        // passar a informação do ficheiro .json para uma variável
+        $list = json_decode($devicesFile, true);
 
         // Array com a informação que queres, por ordem
-// nota: Lista de detalhes no site da phonedb.net
-// exemplo: https://phonedb.net/index.php?m=device&id=18196&c=samsung_sm-a725fds_galaxy_a72_2021_premium_edition_dual_sim_td-lte_emea_128gb__samsung_a725
+        // nota: Lista de detalhes no site da phonedb.net
+        // exemplo: https://phonedb.net/index.php?m=device&id=18196&c=samsung_sm-a725fds_galaxy_a72_2021_premium_edition_dual_sim_td-lte_emea_128gb__samsung_a725
         $wanted = array("Brand", "Model", "Codename", "OEM ID", "Resolution");
 
-// objecto cURL
+        // array associativo para guardar os dados
+        $response = [];
+
+        // objecto cURL
         $phoneObj = new curlPhone;
 
-
-// array associativo para guardar os dados
-        $response = [];
-    }
-
-    private function runJson()
-    {
         foreach ($list as $obj) {
             // consome o url e fornece o html
             if (trim($obj["device_hardware_model"]) !== "null") {
@@ -73,7 +68,6 @@ class phoneScript
                                             $info = $t->getElementsByTagName("td")[1]->nodeValue;
                                         }
 
-
                                         // preenchimento do array associativo
                                         $response[$toAdd][$value] = $info;
 
@@ -98,7 +92,120 @@ class phoneScript
         fwrite($fp, json_encode($response));
         fclose($fp);
 
-        echo "Resultado: " . count($response) . "/" . count($list);
+        echo "Resultado: " . count($response) . "/" . count($list) . PHP_EOL;
     }
 
+    // Sort dos dispositivos rejeitados e aceites
+    public function matchingPercentage(){
+        // ficheiro json na mesma diretoria do script com os modelos
+        $str = file_get_contents('./results.json');
+
+        // passar a informação do ficheiro .json para uma variável
+        $response = json_decode($str, true);
+
+        // array de rejeitados (não foram encontrados no phonedb.net)
+        $rejected = [];
+
+        // atributos a utilizar no matching
+        $attributesToSearch = ["Model", "Codename", "OEM ID"];
+
+        foreach ($response as $model => $items) {
+            $secondSearch = str_replace(["-", "_"], ["/", "/"], $model);
+            if ($secondSearch !== $model) {
+                $secondSearch = substr($secondSearch, strpos($secondSearch, "/") + 1);
+            }
+
+            $thirdSearch = str_replace(["-", "_"], [" ", " "], $model);
+
+            $piecesFromModel = explode(" ", $thirdSearch);
+
+            $numberOfWordsInModel = count($piecesFromModel);
+            $prcnt = 0;
+            $countSimilarWords = 0;
+
+            // correr cada item dentro do model
+            foreach ($items as $item => $i) {
+                if (in_array($item, $attributesToSearch)) {
+
+                    if ($this->searchForNeedle($model, $i) !== false or $this->searchForNeedle($secondSearch, $i) !== false) {
+                        $prcnt = 100;
+                        break;
+                    }
+                    if(count($piecesFromModel) === 0){
+                        break;
+                    }
+
+                    $piecesFromItem = explode(" ", $i);
+                    foreach ($piecesFromModel as $modelWord) {
+                        foreach ($piecesFromItem as $itemWord) {
+                            if ($this->searchForNeedle(trim($modelWord), trim($itemWord)) !== false) {
+                                unset($piecesFromModel[array_search($modelWord, $piecesFromModel)]);
+                                $countSimilarWords++;
+                            }
+                        }
+                    }
+                }
+            }
+            // se percentagem ainda não existe ou for maior que a existente
+            if (@$response[$model]["Matching Percentage"] === null or @$response[$model]["Matching Percentage"] < $prcnt) {
+                if ($countSimilarWords !== 0) { // se só encontrou por count de palavras
+                    $prcnt = ($countSimilarWords / $numberOfWordsInModel) * 100;
+                }
+                if($prcnt >= 60){
+                    $response[$model]["Matching Percentage"] = $prcnt;
+                }
+
+                else{
+                    $rejected[$model] = $response[$model];
+                    unset($response[$model]);
+                }
+            }
+        }
+        $fp = fopen('afterMatching.json', 'w');
+        fwrite($fp, json_encode($response));
+        fclose($fp);
+
+        $fp = fopen('rejected.json', 'w');
+        fwrite($fp, json_encode($rejected));
+        fclose($fp);
+        echo "-------------------------".PHP_EOL;
+        echo "Matching:".PHP_EOL;
+        echo "Resultado: ".count($response)."/".(count($response)+count($rejected)) . PHP_EOL;
+    }
+
+    // Verifica quais dos dispositivos aceites não têm as resoluções mínimas
+    public function checkRes(){
+        // ficheiro json na mesma diretoria do script com os modelos
+        $str = file_get_contents('./afterMatching.json');
+
+        // passar a informação do ficheiro .json para uma variável
+        $response = json_decode($str, true);
+
+        // array com os dispositivos com resoluções menores que as mínimas requeridas
+        $tooSmall = [];
+
+        // Procuramos dispositivos com resoluções menores que 640x1136 (tanto altura como largura)
+        foreach ($response as $model){
+            $aStrings = explode("x", $model["Resolution"]);
+
+            if((int)$aStrings[0] < 640 OR (int)$aStrings[1] < 1136){
+                array_push($tooSmall, $model);
+            }
+        }
+
+        $fp = fopen('tooSmall.json', 'w');
+        fwrite($fp, json_encode($tooSmall));
+        fclose($fp);
+
+        echo "------------------------".PHP_EOL;
+        echo "Resultado (Dispositivos c/ Res Pequena / Total de Dispositivos Verificados): ".count($tooSmall)."/".count($response) . PHP_EOL;
+    }
+
+    private function searchForNeedle($pNeedle, $pHaystack)
+    {
+        $needle = strtoupper($pNeedle);
+        $haystack = strtoupper($pHaystack);
+
+        return strpos($haystack, $needle);
+    }
 }
--- run_this.php
+++ run_this.php
@@ -1,1 +1,20 @@
 <?php
+// Este script foi feito em apenas um dia (peca de muito) e funciona apenas para o sítio phonedb.net
+// nota: não havia acesso a APIs que ajudassem a este problema significativamente
+
+require_once "phoneScript.php";
+
+// json: unidimensional e ter pelo menos um dos seguintes attr: "device_model_name" ou "device_hardware_model"
+// exemplo no ficheiro devices.json
+
+$phoneDB = new phoneScript();
+
+// escreve os dados "scrapped" de phonedb.net para o ficheiro "results.json"
+$phoneDB->runJsonForPhoneDB("./devices.json");
+
+// os dispositivos encontrados em phonedb.net poderão não ser os que se procura, separam-se em "rejected.json" e
+// "afterMatching.json" os rejeitados e os aceites respetivamente
+$phoneDB->matchingPercentage();
+
+// Vê quais os dispositivos que não têm a resolução mínima
+$phoneDB->checkRes();
